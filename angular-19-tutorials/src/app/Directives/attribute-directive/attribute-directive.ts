085ef24935495993cea02f5ca25faa3fb2cc9ab9
Ghadage Rahul <inb322rahgha@ext.crifnet.com>
2025-11-01 22:20:10 +0530

    learn project commit

---
+++ angular-19-tutorials/src/app/Directives/attribute-directive/attribute-directive.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attribute-directive',
+  imports: [],
+  templateUrl: './attribute-directive.html',
+  styleUrl: './attribute-directive.css',
+})
+export class AttributeDirective {
+
+}
---
+++ angular-19-tutorials/src/app/Directives/component-directive/component-directive.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-directive',
+  imports: [],
+  templateUrl: './component-directive.html',
+  styleUrl: './component-directive.css',
+})
+export class ComponentDirective {
+
+}
+//The decorator annotated with @Component is called as component directive
+//Every component you write is directive. 
