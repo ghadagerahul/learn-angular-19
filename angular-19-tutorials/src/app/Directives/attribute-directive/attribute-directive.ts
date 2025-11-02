36e605388901d1deb29a4b69c390b59d580be545
Ghadage Rahul <inb322rahgha@ext.crifnet.com>
2025-11-02 22:34:45 +0530

    commit chanegs

--- angular-19-tutorials/src/app/Directives/attribute-directive/attribute-directive.ts
+++ angular-19-tutorials/src/app/Directives/attribute-directive/attribute-directive.ts
@@ -1,11 +1,49 @@
+import { CommonModule, NgClass, NgStyle } from '@angular/common';
+import { Xliff } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-attribute-directive',
-  imports: [],
+  imports: [FormsModule, NgClass, CommonModule, NgStyle],
   templateUrl: './attribute-directive.html',
   styleUrl: './attribute-directive.css',
 })
 export class AttributeDirective {
+
+
+
+  //NgClass : ngclass is used to chnage eternal CSS of the DOM stucture
+  //Example-1
+  colorVal: string = '';
+  chnageColor(color: string) {
+    this.colorVal = color;
+  }
+
+  //Example-2: checkbox
+  clolorFlag: boolean = false;
+
+
+  //Example-3 : inputfiels
+  colorName: string = '';
+
+  //ngStyle:    ng style is use to change internal CSS of the DOM  strucutre.
+  //Example-1
+  styleColorEx1: string = 'orange';
+  chnageStyleColor(x: string) {
+    this.styleColorEx1 = x;
+  }
+
+  //Example-2: checkbox
+  styleColor: boolean = false;
+  updateStylecolorcheckbox(x: boolean) {
+    if (x) {
+      this.styleColor = true;
+    }
+  }
+
+  //Example-3:input
+
+  textColor: string = '';
 
 }
--- angular-19-tutorials/src/app/app.ts
+++ angular-19-tutorials/src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StructuralDirective } from "./Directives/structural-directive/structural-directive";
+import { AttributeDirective } from "./Directives/attribute-directive/attribute-directive";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, StructuralDirective, AttributeDirective],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular-19-tutorials');
+
+
 }
